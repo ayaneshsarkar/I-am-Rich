db4f355c3ebcb08abb26fc69dbad115fb1b27fbd
Ayanesh Sarkar <ayaneshsarkar101@gmail.com>
2024-01-21 13:46:19 +0530

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Ayanesh Sarkar on 21/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
